1878098e800b7be10bff749b98c2df5b84f06c8f
Ronualdo Maciel <raxmac@gmail.com>
2017-10-29 16:05:11 -0300

    Cleanup on moviecom_crawler module
    Removes unecessary methods

--- lib/guido/crawlers/moviecom_crawler.ex
+++ lib/guido/crawlers/moviecom_crawler.ex
@@ -1,12 +1,12 @@
+require IEx;
+
 defmodule Guido.Crawlers.MoviecomCrawler do
   def extract_data(_date, gateway) do
     gateway.()
     |> parse
   end
 
-  defp parse(input={:error, _}) do
-    input
-  end
+  defp parse(input={:error, _}), do: input
 
   defp parse({:ok, content}) do
     content
@@ -15,59 +15,40 @@ defmodule Guido.Crawlers.MoviecomCrawler do
     |> Enum.into(%{})
   end
 
-  defp parse_line(line) do
-    {extract_movie_title(line), extract_sessions(line)}
+  defp parse_line({_tr, _attrs, table_tds}) do
+    {extract_movie_title(hd(table_tds)), extract_sessions(tl(table_tds))}
   end
 
-  defp extract_movie_title({_elem, _attrs, children}) do
-    children
-    |> hd
+  defp extract_movie_title(title_td_element) do
+    title_td_element
     |> Floki.find("td.tabela-linha>div>div.programcao-filme-info>div.tit>a")
     |> Floki.text
     |> String.trim
   end
 
-  defp extract_sessions({_elem, _attrs, children}) do
-    children
-    |> second_element
+  defp extract_sessions(session_tds) do
+    session_tds
     |> Floki.find("td.tabela-linha>table>tbody>tr")
     |> Enum.map(&extract_session/1)
   end
 
-  defp extract_session(session_line) do
-    {extract_session_type(session_line), extract_schedule(session_line)} 
+  defp extract_session({_tr, _attrs, table_tds}) do
+    {extract_session_type(hd(table_tds)), extract_schedule(tl(table_tds))} 
   end
 
-  defp second_element(list) do
-    [_first | [second | _tail]] = list
-    second
+  defp extract_session_type(session_type_td) do
+    session_type_td
+    |> extract_text_from("td>div.icon-tipo")
   end
 
-  defp third_element(list) do
-    [_first | [_second | tail]] = list
-    tail
+  defp extract_schedule(schedule_td) do
+    schedule_td
+    |> extract_text_from("div.icon-hora>a")
   end
 
-  defp extract_session_type(session_line) do
-    session_line
-    |> Floki.find("td:first-child")
-    |> Enum.map(&(&1 |> Floki.find("td>div.icon-tipo")))
-    |> Enum.flat_map(&extract_type/1)
-  end
-
-  defp extract_type([]) do
-    [""]
-  end
-
-  defp extract_type(div) do
-    div
-    |> Enum.map(&Floki.text/1)
-  end
-
-  defp extract_schedule({_elem, _attrs, children}) do
-    children
-    |> third_element
-    |> Floki.find("div.icon-hora>a")
+  defp extract_text_from(input, selector) do
+    input
+    |> Floki.find(selector)
     |> Enum.map(&Floki.text/1)
   end
 end
